d7e158f2b90d46e0605603188787cc75c11071ec
samiyaazad <samiya16azad@gmail.com>
2026-02-13 19:06:37 +0600

    fix: resolved API, caching, and star animation issues
    Fix API key integration, correct caching variable errors, and improve star background animation for smooth infinite movement.

--- script.js
+++ script.js
@@ -1,49 +1,114 @@
-document.getElementById("loadBtn").addEventListener("click", getSpace);
+// State Management 
+let isLoading = false;
 
+//Event Listener 
+document.getElementById("loadBtn").addEventListener("click",getSpace);
+
+// Auto-load today's APOD 
+getSpace();
+
+// Main Fetch Function 
 async function getSpace() {
+
+    if (isLoading) return;
+
+    const status = document.getElementById("status");
+    const button = document.getElementById("loadBtn");
+
     try {
+        isLoading = true;
+        button.disabled = true;
+        status.innerText = "Fetching data from NASA...";
+
         const selectedDate = document.getElementById("datePicker").value;
-        let url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        const today = new Date().toISOString().split("T")[0];
 
-        if (selectedDate) url += "&date=" + selectedDate;
-
-        const response = await fetch(url);
-        if (!response.ok) throw new Error("Failed to fetch Nasa data");
-
-        const data = await response.json();
-
-        document.getElementById("title").innerText = data.title;
-        document.getElementById("date").innerText = "Date: " + data.date;
-        document.getElementById("explanation").innerText = data.explanation;
-
-        const container = document.getElementById("mediaContainer");
-        container.innerHTML = "";
-
-        if (data.media_type === "image") {
-            const img = document.createElement("img");
-            img.src = data.url;
-
-            img.classList.remove("fade-in");
-            void img.offsetWidth;
-            img.classList.add("fade-in");
-
-            container.appendChild(img);
-
-        } else if (data.media_type === "video")  {
-            const iframe = document.createElement("iframe");
-            iframe.src = data.url;
-            iframe.width = "100%";
-            iframe.height = "500px";
-            iframe.frameBorder = "0";
-            iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-            iframe.allowFullScreen = true;
-            container.appendChild(iframe);
+        // Prevent Future date selection 
+        if (selectedDate && selectedDate > today ) {
+            throw new Error("NASA hasn't published future APODs yet.");
         }
 
+        const API_KEY = "3p7bmb9KW0ObWpsVT0YUFZogJ2EAgXd5CK7UG8X3";
+        let url = "https://api.nasa.gov/planetary/apod?api_key=3p7bmb9KW0ObWpsVT0YUFZogJ2EAgXd5CK7UG8X3";
+
+        if (selectedDate) {
+            url += "&date=" + selectedDate;
+        }
+
+        // Caching Layer 
+
+        const cacheKey = `apod-${selectedDate || "today"}`;
+        const cachedDate = localStorage.getItem(cacheKey);
+
+        let data;
+
+        if (cachedDate) {
+            data = JSON.parse(cachedDate);
+            console.log("Loaded from cache");
+        } else {
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error("Network response failed.");
+            }
+
+            data = await response.json();
+            localStorage.setItem(cacheKey, JSON.stringify(data));
+            console.log("Fetched from NASA API");
+
+        }
+
+        renderData(data);
+        status.innerText = "";
+
+
     } catch (error) {
-        alert("Oops! Something went wrong: " + error.message);
+        status.innerText = error.message;
         console.error(error);
+    } finally {
+        isLoading = false;
+        button.disabled = false;
     }
+
 }
 
-getSpace();
+
+// Render Function (Separation of Concerns) 
+
+function renderData(data) {
+     
+    const title = document.getElementById("title");
+    const date = document.getElementById("date");
+    const explanation = document.getElementById("explanation");
+    const container = document.getElementById("mediaContainer");
+
+    title.innerText = data.title;
+    date.innerText = "Date: " + data.date;
+    explanation.innerText = data.explanation;
+
+    container.innerHTML = "";
+
+    if (data.media_type === "image") {
+
+        const img = document.createElement("img");
+        img.src = data.url;
+        img.classList.add("fade-in");
+
+        container.appendChild(img);
+
+    } else if (data.media_type === "video") {
+
+        const iframe = document.createElement("iframe");
+        iframe.src = data.url;
+        iframe.width = "100%";
+        iframe.height = "500px";
+        iframe.frameBorder = "0";
+        iframe.allow = 
+                "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+        iframe.allowFullscreen = true;
+
+        container.appendChild(iframe);
+
+    }
+
+}
